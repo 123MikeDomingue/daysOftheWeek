c6386324f1cfa0375ae9093fc37395a339c07f86
123MikeDomingue <mikedomingue543216@gmail.com>
2022-07-13 10:30:43 -0400

    added the javascript file answering the question

---
+++ java.js
@@ -0,0 +1,33 @@
+let day = 'saturday';
+
+
+switch(day) {
+ case 'monday':
+ console.log('That is the wrong day');
+ break;
+ case 'tuesday':
+ console.log('That is the wrong day');
+ break;
+ case 'wednesday':
+ console.log('That is the wrong day');
+ break;
+ case 'thursday':
+ console.log('That is the wrong day');
+ break;
+ case 'friday':
+ console.log('That is the wrong day');
+ break;
+ 
+ case 'monday':
+ console.log('That is the wrong day');
+ break;
+
+ default:
+  console.log(` The correct day is ${day}`);
+  break;
+  
+}
+
+
+
+
